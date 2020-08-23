fd9f6702d448b252aebb908f374e2de203590082
Tom Skypala <xskypala@gmail.com>
2020-08-24 01:14:12 +0200

    test: add basevc and scenedelegate tests

--- Tests/BaseAppCoordinationTests/BaseAppCoordinationTests.swift
+++
@@ -1,5 +0,0 @@
-import XCTest
-@testable import BaseAppCoordination
-
-final class BaseAppCoordinationTests: XCTestCase {
-}
---
+++ Tests/BaseAppCoordinationTests/BaseViewControllerTests.swift
@@ -0,0 +1,41 @@
+//
+//  BaseViewControllerTests.swift
+//
+//  Created by Tomas Skypala on 09/03/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import XCTest
+@testable import BaseAppCoordination
+
+private final class TestableScreenView: UIView, BaseScreenView {
+
+    var didReceiveSetupSubviews: Bool = false
+
+    func setupSubviews() {
+        didReceiveSetupSubviews = true
+    }
+}
+
+private final class TestableBaseViewController: BaseViewController<TestableScreenView> { }
+
+final class BaseViewControllerTests: XCTestCase {
+
+    private var baseViewController: TestableBaseViewController!
+
+    override func setUp() {
+        baseViewController = .init()
+    }
+
+    func testLoadViewInitsScreenViewWithCorrectType() {
+        baseViewController.loadView()
+
+        XCTAssertTrue(TestableScreenView.self == type(of: baseViewController.screenView!))
+    }
+
+    func testLoadViewDoesCallSetupSubviewsOnScreenView() {
+        baseViewController.loadView()
+
+        XCTAssertTrue(baseViewController.screenView!.didReceiveSetupSubviews)
+    }
+}
